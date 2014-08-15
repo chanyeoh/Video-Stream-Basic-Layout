8d348ef9dc130d6baf3fd5c575d63381a5d0bb2e
chanyeoh <yeohchan@Yeohs-MacBook-Air.local>
2014-08-15 10:54:16 +0800

    Conflict ViewController Fix
    Conflicts:
    stream/ViewController.m

--- stream/ViewController.m
+++ stream/ViewController.m
@@ -23,6 +23,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
     self.title = @"Loading...";
     OneDriveFileRetrival *oneDrive = [[OneDriveFileRetrival alloc]init];
     [oneDrive getFileList:^(NSArray *fileList, NSString *keywordsText, NSError *error) {
@@ -47,6 +48,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+-(void)keywordAlgorithm
+{
+    //NSMutableDictionary
+}
+
+
+#pragma mark -
+#pragma mark Table View Delegates
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_videoArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [_videoArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 
 
@@ -82,77 +113,56 @@
 
 
 /*
--(void)cutImage:(NSURL *)assetURL{
-    int startMilliseconds = (0 * 1000);
-    int endMilliseconds = (1 * 1000);
+ -(void)cutImage:(NSURL *)assetURL{
+ int startMilliseconds = (0 * 1000);
+ int endMilliseconds = (1 * 1000);
  
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    
-    NSFileManager *manager = [NSFileManager defaultManager];
-    
-    NSString *outputURL = [documentsDirectory stringByAppendingPathComponent:@"output"] ;
-    [manager createDirectoryAtPath:outputURL withIntermediateDirectories:YES attributes:nil error:nil];
-    
-    outputURL = [outputURL stringByAppendingPathComponent:@"output1.mp4"];
-    [manager removeItemAtPath:outputURL error:nil];
-    
-    AVURLAsset *videoAsset = [AVURLAsset URLAssetWithURL:assetURL options:nil];
-    
-    
-    
-    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:videoAsset presetName:AVAssetExportPresetHighestQuality];
-    exportSession.outputURL = [[NSURL alloc] initFileURLWithPath:outputURL isDirectory:true] ;
-    exportSession.outputFileType = AVFileTypeQuickTimeMovie;
-    CMTimeRange timeRange = CMTimeRangeMake(CMTimeMake(startMilliseconds, 1000), CMTimeMake(endMilliseconds - startMilliseconds, 1000));
-    exportSession.timeRange = timeRange;
-    
-    [exportSession exportAsynchronouslyWithCompletionHandler:^{
-        switch (exportSession.status) {
-            case AVAssetExportSessionStatusCompleted:
-                // Custom method to import the Exported Video
-                //[self loadAssetFromFile:exportSession.outputURL];
-                //NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@", videoURL]];
-                NSLog(@"HERE");
-                //_mc.initialPlaybackTime
-    
-                break;
-            case AVAssetExportSessionStatusFailed:
-                //
-                NSLog(@"Failed:%@",exportSession.error);
-                break;
-            case AVAssetExportSessionStatusCancelled:
-                //
-                NSLog(@"Canceled:%@",exportSession.error);
-                break;
-            default:
-                NSLog(@"Error");
-                break;
-        }
-    }];
-}
+ NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+ NSString *documentsDirectory = [paths objectAtIndex:0];
+ 
+ NSFileManager *manager = [NSFileManager defaultManager];
+ 
+ NSString *outputURL = [documentsDirectory stringByAppendingPathComponent:@"output"] ;
+ [manager createDirectoryAtPath:outputURL withIntermediateDirectories:YES attributes:nil error:nil];
+ 
+ outputURL = [outputURL stringByAppendingPathComponent:@"output1.mp4"];
+ [manager removeItemAtPath:outputURL error:nil];
+ 
+ AVURLAsset *videoAsset = [AVURLAsset URLAssetWithURL:assetURL options:nil];
+ 
+ 
+ 
+ AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:videoAsset presetName:AVAssetExportPresetHighestQuality];
+ exportSession.outputURL = [[NSURL alloc] initFileURLWithPath:outputURL isDirectory:true] ;
+ exportSession.outputFileType = AVFileTypeQuickTimeMovie;
+ CMTimeRange timeRange = CMTimeRangeMake(CMTimeMake(startMilliseconds, 1000), CMTimeMake(endMilliseconds - startMilliseconds, 1000));
+ exportSession.timeRange = timeRange;
+ 
+ [exportSession exportAsynchronouslyWithCompletionHandler:^{
+ switch (exportSession.status) {
+ case AVAssetExportSessionStatusCompleted:
+ // Custom method to import the Exported Video
+ //[self loadAssetFromFile:exportSession.outputURL];
+ //NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@", videoURL]];
+ NSLog(@"HERE");
+ //_mc.initialPlaybackTime
+ 
+ break;
+ case AVAssetExportSessionStatusFailed:
+ //
+ NSLog(@"Failed:%@",exportSession.error);
+ break;
+ case AVAssetExportSessionStatusCancelled:
+ //
+ NSLog(@"Canceled:%@",exportSession.error);
+ break;
+ default:
+ NSLog(@"Error");
+ break;
+ }
+ }];
+ }
  */
-
-#pragma mark -
-#pragma mark Table View Delegates
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [_videoArray count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *simpleTableIdentifier = @"SimpleTableItem";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-    
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
-    }
-    
-    cell.textLabel.text = [_videoArray objectAtIndex:indexPath.row];
-    return cell;
-}
 
 
 @end
